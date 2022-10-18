4f64ed34cfb86500c4bbd998e6c611ac891d2544
duclm <leminhduc22072000@gmail.com>
2022-10-18 16:50:47 +0700

    leminhduc22072003/ss4-lab1

--- src/seesion4lab1/Main.java
+++ src/seesion4lab1/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args){
         PhoneBook pb1 = new PhoneBook();
 
-        pb1.insertPhone("Duc","0359903359");
+        pb1.insertPhone("Cam","0359903359");
         pb1.insertPhone("Linh","0904175005");
-        pb1.insertPhone(" Cường","012345678");
+        pb1.insertPhone("Lê Cường","012345678");
         pb1.insertPhone("Duy","023456789");
 
         System.out.println("\nDisplay contact list\n");
@@ -15,7 +15,7 @@ public class Main {
             System.out.println(ct.phone);
         }
 
-        pb1.removePhone("Cường");
+        pb1.removePhone("Lê Cường");
         System.out.println("\nRemove\n");
         for(contact ct: pb1.PhoneList){
             System.out.println(ct.name);
@@ -23,7 +23,7 @@ public class Main {
         }
 
         System.out.println("\nUpdate\n");
-        pb1.updatePhone(" Cường","012345678");
+        pb1.updatePhone("Lê Cường","012345678");
         pb1.updatePhone("Duy","034567890");
 
         for(contact ct: pb1.PhoneList){
